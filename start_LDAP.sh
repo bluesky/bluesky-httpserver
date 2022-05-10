d5915762a6d25d8cd367c66a4f16b513ee3b5b54
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2022-05-10 11:26:15 -0400

    ENH: start LDAP server using a separate script

---
+++ start_LDAP.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+# Start LDAP server in docker container
+sudo docker pull bitnami/openldap:latest
+sudo docker-compose -f .github/workflows/docker-configs/ldap-docker-compose.yml up -d
+sudo docker ps
