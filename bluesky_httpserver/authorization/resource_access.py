caf42ec28f6721e6491aa4ea1e7f560d1306d8ab
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2022-09-23 18:18:07 -0400

    DOC: docstrings for 'DefaultResourceAccessControl'

--- bluesky_httpserver/authorization/resource_access.py
+++ bluesky_httpserver/authorization/resource_access.py
@@ -19,6 +19,34 @@ properties:
 
 
 class DefaultResourceAccessControl:
+    """
+    The resource access policy associates users with user groups. The groups
+    define the resources, such as plans and devices users can access. The
+    default policy assumes that all uses belong to a singe group (default user group).
+    The name of the group is returned by ``get_resource_name()`` method for any
+    submitted ``username``. The hard-coded name for the default user group can be
+    modified by passing the parameter ``default_group`` to the class constructor.
+    The arguments of the class constructor are specified in the configuration
+    file as shown in the example below.
+
+    Parameters
+    ----------
+    default_group: str
+        The name of the group returned by the access manager by default.
+
+    Examples
+    --------
+    Configure ``DefaultResourceAccessControl`` policy to use different group
+    name for all the users. The new default group name is ``test_user``.
+
+    .. code-block::
+
+        resource_access:
+            policy: bluesky_httpserver.authorization:DefaultResourceAccessControl
+            args:
+                default_group: test_user
+    """
+
     def __init__(self, *, default_group=None):
         try:
             config = {"default_group": default_group}
@@ -34,4 +62,17 @@ class DefaultResourceAccessControl:
         self._default_group = default_group
 
     def get_resource_group(self, username):
+        """
+        Returns the name of the user group based on the user name.
+
+        Parameters
+        ----------
+        username: str
+            User name.
+
+        Returns
+        -------
+        str
+            Name of the user group.
+        """
         return self._default_group
