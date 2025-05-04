1b4a8c2bb1d0a2c01612cc5b211cfb72062ed7b1
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2025-05-04 16:39:14 -0400

    DOC: commented out matplotlib extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'IPython.sphinxext.ipython_directive',
     'IPython.sphinxext.ipython_console_highlighting',
-    'matplotlib.sphinxext.plot_directive',
+    # 'matplotlib.sphinxext.plot_directive',
     'numpydoc',
 ]
 
