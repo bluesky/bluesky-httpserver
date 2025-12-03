d599ed30db8c7e8c6f6e4793157ccc28d9af0b21
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2025-12-03 15:18:26 -0500

    TST: potential fix for LDAP tests

--- start_LDAP.sh
+++ start_LDAP.sh
@@ -3,6 +3,6 @@
 set -e
 
 # Start LDAP server in docker container
-sudo docker pull osixia/openldap:latest
+# sudo docker pull osixia/openldap:latest
 sudo docker compose -f .github/workflows/docker-configs/ldap-docker-compose.yml up -d
 sudo docker ps
